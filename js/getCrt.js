aae5bad0f3b1b7b1f5ed5f4b41a012ac609a779a
ggugguiscute <kimgukwoo@gmail.com>
2023-02-01 10:16:47 +0900

    0201 search bar input

--- js/getCrt.js
+++ js/getCrt.js
@@ -11,8 +11,8 @@ function success(position) {
   //   console.log("경도: " + position.coords.longitude);
   lat = crd.latitude;
   lon = crd.longitude;
-  console.log(lat);
-  console.log(lon);
+  // console.log(lat);
+  // console.log(lon);
 
   getCrtNm(lat, lon);
 }
@@ -35,11 +35,24 @@ function getCrtNm(latitude, longitude) {
       Authorization: "KakaoAK 1862c352cbdd230df957e832830029bc", //승인 rest api key
     },
     success: function (data) {
-      console.log(data.documents[0].address.region_2depth_name);
+      // console.log(data.documents[0].address.region_2depth_name);
       $(".crt-gu").text(data.documents[0].address.region_2depth_name);
+
+      //지역 버튼 클릭 시 데이터 전달
+      $(".near-btns button").on("click", function () {
+        const region = $(".crt-gu").text();
+        // console.log(region);
+        location.href = `/ev_charge/pages/search_list.html?key=${region}`;
+      });
     },
     error: function (e) {
       console.log(error);
     },
   });
 }
+
+const searchBtn = document.querySelector(".search-bar button");
+searchBtn.addEventListener("click", function () {
+  const searchParam = document.querySelector(".search-bar input").value;
+  location.href = `/ev_charge/pages/search_list.html?key=${searchParam}`;
+});
